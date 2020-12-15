777da9149fb734c3375b3a39c118e2c575612399
Ahmad Siddiq <60493382+ahmadsiddiq-lang@users.noreply.github.com>
2020-12-15 09:32:23 +0700

    set font

---
+++ assets/fonts/index.js
@@ -0,0 +1,16 @@
+
+export const Poppins = {
+    Regular: 'Poppins-Regular',
+    Bold: 'Poppins-Bold',
+    BoldItalic: 'Poppins-BoldItalic',
+    ExtraBold: 'Poppins-ExtraBold',
+    ExtraBoldItalict: 'Poppins-ExtraBold',
+    Italic: 'Poppins-Italic',
+    Medium: 'Poppins-Medium',
+    MediumItalic: 'Poppins-MediumItalic',
+};
+
+export const Dancing = {
+    Regular: 'DancingScript-Regular',
+    Bold: 'DancingScript-Bold',
+};
