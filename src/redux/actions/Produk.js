afc2740fb4bc8f169a601623c7da1b2c974edf9c
Ahmad Siddiq <60493382+ahmadsiddiq-lang@users.noreply.github.com>
2021-01-04 18:11:55 +0700

    revisi

--- src/redux/actions/Produk.js
+++ src/redux/actions/Produk.js
@@ -2,12 +2,11 @@ import Axios from 'axios';
 import { BASE_URL } from '../../config/URL';
 export const GET_PRODUK = 'GET_PRODUK';
 
-export const getProduk = (setVisible) => {
+export const getProduk = () => {
     return async (dispatch) => {
         await Axios.get(BASE_URL + 'product', {
             withCredentials: true,
         }).then(dataProduk => {
-            setVisible(true);
             dispatch({
                 type: GET_PRODUK,
                 data: dataProduk.data.data.product,
